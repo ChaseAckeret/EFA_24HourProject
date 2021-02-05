b478a4c6d3d24e7c82c97a886c45c927ee3270a5
JForestal1 <jforestal1@gmail.com>
2021-02-04 21:16:14 -0500

    Comment class complete (3.03)

---
+++ 24HourProject.Data/Comment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24HourProject.Data
+{
+    class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string CommentText { get; set; }
+
+        [Required]
+        public Guid Author { get; set; }
+
+        [Display(Name = "Date Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        [Display(Name = "Date Last Modified")]
+        public DateTimeOffset? ModifiedUtc { get; set; }
+
+
+        /// rough in for Replies
+
+        //public virtual List<Reply> Reply { get; set; }
+
+        //[ForeignKey(nameof(Reply))]
+
+        //public int ReplyId { get; set; }
+
+    }
+}
