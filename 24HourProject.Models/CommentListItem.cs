377841081ca9de338bb2f717c998bdd277864518
JForestal1 <jforestal1@gmail.com>
2021-02-04 21:38:48 -0500

    Create Comment added to service

--- 24HourProject.Models/CommentListItem.cs
+++ 24HourProject.Models/CommentListItem.cs
@@ -1,36 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _24HourProject.Data
+namespace _24HourProject.Models
 {
-    class Comment
+    public class CommentListItem
     {
-        [Key]
         public int Id { get; set; }
-
-        [Required]
+        
         public string CommentText { get; set; }
 
-        [Required]
-        public Guid Author { get; set; }
-
+        [Display(Name = "Date Created")]
         public DateTimeOffset CreatedUtc { get; set; }
 
+        [Display(Name = "Date Last Modified")]
         public DateTimeOffset? ModifiedUtc { get; set; }
-
-
-        /// rough in for Replies
-
-        //public virtual List<Reply> Reply { get; set; }
-
-        //[ForeignKey(nameof(Reply))]
-
-        //public int ReplyId { get; set; }
-
     }
 }
